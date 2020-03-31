5cd7aea24291fd670cb451f051b143b41530bd00
upendra <joshiupendra1995>
2020-03-31 16:54:49 +0530

    calculator app initial commit

---
+++ src/test/java/com/uj/calculator/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uj.calculator.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
